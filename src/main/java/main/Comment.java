2d384108e3c0b07f5312346c9304159afcb766ae
maxim19780306 <rm83788@gmail.com>
2022-09-06 00:03:48 +0300

    implementation of a simple aspect

---
+++ src/main/java/main/Comment.java
@@ -0,0 +1,9 @@
+package main;
+
+import lombok.Data;
+
+@Data
+public class Comment {
+    private String text;
+    private String author;
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
+
+public class Main {
+    public static void main(String[] args) {
+       var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+       var service = context.getBean(CommentService.class);
+
+       Comment comment = new Comment();
+       comment.setText("Demo comment");
+       comment.setAuthor("Maxim");
+       service.publishComment(comment);
+    }
+}
